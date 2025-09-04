d44e9917d183c0e1b86ac1c307c83b579a9e61bc
Anthony Baker <anthonybaker.sd@gmail.com>
2025-09-03 20:57:50 -0700

    update images

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
-  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH + "/" : "",
+  basePath: "/anthony-baker-portfolio",
+  assetPrefix: "/anthony-baker-portfolio/",
   images: {
     unoptimized: true,
     domains: ["avatars.githubusercontent.com"],
