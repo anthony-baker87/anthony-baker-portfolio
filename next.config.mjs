af7b42d4d2140fcbef7e9454d99fc9881a840e4a
Anthony Baker <anthonybaker.sd@gmail.com>
2025-09-06 15:00:43 -0700

    update config for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
+// Ensure assetPrefix always has a trailing slash if basePath exists
+const assetPrefix = isProd && basePath ? `${basePath}/` : "";
+
 const nextConfig = {
   output: "export",
-  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
-  assetPrefix: isProd ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : "",
+  basePath: isProd ? basePath : "",
+  assetPrefix,
   images: {
     unoptimized: true,
   },
